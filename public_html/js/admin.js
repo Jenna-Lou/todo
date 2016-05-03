4c52f350a5af29581eebbdaca563bae9cb242eaf
Jenna Lou <jennalouronquillo@yahoo.com>
2016-05-02 17:54:33 -0700

    changing color

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -104,3 +104,14 @@ function gotError(error) {
     console.log("Error code - " + error.code);
     Materialize.toast('Incorrect!', 4000);
 }
+
+$('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
